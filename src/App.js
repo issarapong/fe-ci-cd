41a803e6422c70eeb4a88722a84d7e49d4df57ae
IS <1588186+issarapong@users.noreply.github.com>
2023-07-12 19:25:55 +0700

    automate build dockerInages deploy revise

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React& Devops With Issarapong
           {process.env.MAGIC_NAME}
         </a>
       </header>
